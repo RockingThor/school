20b9a443c49fc42b8f60daac81c373aba0d6c979
Rohit Nandi <rohitnandibmp@gmail.com>
2023-12-25 00:09:28 +0530

    after form ui

--- app/(main)/results/[class]/page.tsx
+++ app/(main)/results/[class]/page.tsx
@@ -1,5 +1,28 @@
 "use client";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface PageProps {
   params: {
@@ -7,8 +30,97 @@ interface PageProps {
   };
 }
 
+const formSchema = z.object({
+  class: z.number().min(5).max(12),
+  rollNumber: z.number().min(1).max(200),
+  section: z.string(),
+});
+
 const Page = ({ params }: PageProps) => {
-  return <div>Page</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      class: Number(params.class),
+      rollNumber: 1,
+      section: "",
+    },
+  });
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen p-2">
+      <div className=" items-center justify-center inline-block border p-2 rounded-md">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="flex items-center justify-center space-x-2">
+              <FormField
+                control={form.control}
+                name="class"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Class</FormLabel>
+                    <FormControl>
+                      <Input placeholder="class" {...field} disabled />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="class"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Section</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Select Section" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectItem {...field} value="A">
+                              A
+                            </SelectItem>
+                            <SelectItem {...field} value="B">
+                              B
+                            </SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex items-center justify-center space-x-3">
+              <FormField
+                control={form.control}
+                name="rollNumber"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center justify-center">
+                      <FormLabel>Roll No.</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Input your roll number"
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
