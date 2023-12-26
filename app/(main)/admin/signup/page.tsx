3cad4cdd8369defc36ac81c81c748f71200f02df
Rohit Nandi <rohitnandibmp@gmail.com>
2023-12-26 21:43:37 +0530

    before apis

---
+++ app/(main)/admin/signup/page.tsx
@@ -0,0 +1,12 @@
+import AuthenticationPage from "@/components/auth";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <AuthenticationPage />
+    </div>
+  );
+};
+
+export default Page;
