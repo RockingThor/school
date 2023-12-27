0704c139a9efc16f438d1ddc34089b0dd7d168b1
Rohit Nandi <rohitnandibmp@gmail.com>
2023-12-27 11:39:12 +0530

    after signup backend

---
+++ app/api/signup/route.ts
@@ -0,0 +1,35 @@
+import { db } from "@/db/db";
+import { NextResponse } from "next/server";
+
+interface User {
+    email: string;
+    password: string;
+}
+export async function POST(req: Request) {
+    try {
+        const { email, password }: User = await req.json();
+        if (!email || !password || password.length < 8) {
+            return new NextResponse("Bad Request", { status: 400 });
+        }
+        const data = await db.admin.findFirst({
+            where: {
+                email,
+            },
+        });
+        if (data) {
+            return new NextResponse("User Already Exists", { status: 400 });
+        }
+        const name = "ABC XYZ";
+        const server = await db.admin.create({
+            data: {
+                email,
+                password,
+                name,
+            },
+        });
+        return NextResponse.json(server);
+    } catch (err) {
+        console.log("[api/signup] Error: ", err);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -10,119 +10,141 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const formSchema = z.object({
-  email: z.string().email({
-    message: "Input must be a valid email",
-  }),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters long" })
-    .max(50, { message: "Password cannot be longer than 50 characters" })
-    .refine((password) => /[A-Z]/.test(password), {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .refine((password) => /[a-z]/.test(password), {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .refine((password) => /\d/.test(password), {
-      message: "Password must contain at least one digit",
-    })
-    .refine((password) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password), {
-      message: "Password must contain at least one special character",
+    email: z.string().email({
+        message: "Input must be a valid email",
     }),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .max(50, { message: "Password cannot be longer than 50 characters" })
+        .refine((password) => /[A-Z]/.test(password), {
+            message: "Password must contain at least one uppercase letter",
+        })
+        .refine((password) => /[a-z]/.test(password), {
+            message: "Password must contain at least one lowercase letter",
+        })
+        .refine((password) => /\d/.test(password), {
+            message: "Password must contain at least one digit",
+        })
+        .refine(
+            (password) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password),
+            {
+                message: "Password must contain at least one special character",
+            }
+        ),
 });
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
 
-  async function onSubmit(data: z.infer<typeof formSchema>) {
-    setIsLoading(true);
+    async function onSubmit(data: z.infer<typeof formSchema>) {
+        setIsLoading(true);
+        const apiUrl = "http://localhost:3000/api/signup";
+        const response = await axios.post(apiUrl, data);
+        setIsLoading(false);
+        if (response.status === 200) {
+            router.push("/admin/home");
+        } else {
+            console.log("User creation failed");
+        }
+    }
 
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }
-
-  return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="sr-only" htmlFor="email">
-                  Email
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    id="email"
-                    placeholder="name@example.com"
-                    type="email"
-                    autoCapitalize="none"
-                    autoComplete="email"
-                    autoCorrect="off"
-                    disabled={isLoading}
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="sr-only" htmlFor="email">
-                  Password
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    id="password"
-                    placeholder="Enter Password"
-                    type="password"
-                    autoCapitalize="none"
-                    autoCorrect="off"
-                    disabled={isLoading}
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign Up with Email
-          </Button>
-        </form>
-      </Form>
-    </div>
-  );
+    return (
+        <div
+            className={cn("grid gap-6", className)}
+            {...props}
+        >
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel
+                                    className="sr-only"
+                                    htmlFor="email"
+                                >
+                                    Email
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        type="email"
+                                        autoCapitalize="none"
+                                        autoComplete="email"
+                                        autoCorrect="off"
+                                        disabled={isLoading}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription></FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel
+                                    className="sr-only"
+                                    htmlFor="email"
+                                >
+                                    Password
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="password"
+                                        placeholder="Enter Password"
+                                        type="password"
+                                        autoCapitalize="none"
+                                        autoCorrect="off"
+                                        disabled={isLoading}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription></FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button disabled={isLoading}>
+                        {isLoading && (
+                            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+                        Sign Up with Email
+                    </Button>
+                </form>
+            </Form>
+        </div>
+    );
 }
